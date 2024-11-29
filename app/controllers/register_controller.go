631afbcf15d5d477a34bb73f0b2b5d1e3039abf0
pkruteev <pkruteev@mail.ru>
2024-11-29 23:50:29 +0300

    Reg2

--- app/controllers/register_controller.go
+++ app/controllers/register_controller.go
@@ -8,43 +8,7 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-// CreateBook func for creates a new User.
-// @Description Create a new book.
-// @Summary create a new book
-// @Tags Book
-// @Accept json
-// @Produce json
-// @Param title body string true "Title"
-// @Param author body string true "Author"
-// @Param book_attrs body models.BookAttrs true "Book attributes"
-// @Success 200 {object} models.Book
-// @Security ApiKeyAuth
-// @Router /v1/book [post]
 func RegisterUser(c *fiber.Ctx) error {
-	// Get now time.
-	// now := time.Now().Unix()
-
-	// Get claims from JWT.
-	// claims, err := utils.ExtractTokenMetadata(c)
-	// if err != nil {
-	// 	// Return status 500 and JWT parse error.
-	// 	return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-	// 		"error": true,
-	// 		"msg":   err.Error(),
-	// 	})
-	// }
-
-	// Set expiration time from JWT data of current book.
-	// expires := claims.Expires
-
-	// Checking, if now time greater than expiration from JWT.
-	// if now > expires {
-	// 	// Return status 401 and unauthorized error message.
-	// 	return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
-	// 		"error": true,
-	// 		"msg":   "unauthorized, check expiration time of your token",
-	// 	})
-	// }
 
 	// Create new UserType struct
 	user := &models.UserType{}
@@ -70,11 +34,6 @@ func RegisterUser(c *fiber.Ctx) error {
 
 	// Create a new validator for a Book model.
 	validate := utils.NewValidator()
-
-	// Set initialized default data for book:
-	// book.ID = uuid.New()
-	// book.CreatedAt = time.Now()
-	// book.BookStatus = 1 // 0 == draft, 1 == active
 
 	//Validate user fields.
 	if err := validate.Struct(user); err != nil {
--- app/models/user_model.go
+++ app/models/user_model.go
@@ -2,21 +2,21 @@ package models
 
 // UserType определяет структуру пользователя
 type UserType struct {
-	UserID         int      `db:"user_id" json:"user_id"`
-	FirstName      string   `db:"first_name" json:"first_name"`
-	PatronymicName string   `db:"patronymic_name" json:"patronymic_name,omitempty"`
-	LastName       string   `db:"last_name" json:"last_name,omitempty"`
-	UserEmail      string   `db:"user_email" json:"user_email"`
-	UserPhone      string   `db:"user_phone" json:"user_phone,omitempty"`
-	UserCompany    []string `db:"user_company" json:"user_company,omitempty"`
-	Password       string   `db:"password" json:"password,omitempty"`
-	UserRole       UserRole `db:"user_role" json:"user_role" validate:"required,dive"`
+	UserID         int    `db:"user_id" json:"user_id"`
+	FirstName      string `db:"first_name" json:"first_name"`
+	PatronymicName string `db:"patronymic_name" json:"patronymic_name,omitempty"`
+	LastName       string `db:"last_name" json:"last_name,omitempty"`
+	UserRole       string `db:"user_role" json:"user_role,omitempty"`
+	UserEmail      string `db:"user_email" json:"user_email"`
+	UserPhone      string `db:"user_phone" json:"user_phone,omitempty"`
+	UserCompany    string `db:"user_company" json:"user_company,omitempty"`
+	Password       string `db:"password" json:"password,omitempty"`
 }
 
 // UserRole определяет роли пользователя
-type UserRole struct {
-	Member   string `json:"member"`
-	Admin    string `json:"admin"`
-	Director string `json:"director"`
-	Manager  string `json:"manager"`
-}
+// type UserRole struct {
+// 	Member   string `json:"member"`
+// 	Admin    string `json:"admin"`
+// 	Director string `json:"director"`
+// 	Manager  string `json:"manager"`
+// }
